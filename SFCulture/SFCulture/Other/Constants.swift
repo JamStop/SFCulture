bf189af07d42ada66f1733abe82eb1dca04bc9e4
JamStop <jimmyyue1@gmail.com>
2015-12-04 22:36:40 -0800

    constants and progress views

---
+++ SFCulture/SFCulture/Other/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 12/4/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import Foundation
+
+class Constants: NSObject {
+    
+    //Singleton Class
+    static let sharedInstance = Constants()
+    
+    //Soundcloud Constants
+    let SC_CLIENT_ID: String = "44c0d423bb154343e1936bbc4528ba65"
+    let SC_CLIENT_SECRET: String = "fa19ba59a6819e820f5653788c1d810c"
+    let SC_REDIRECT_URI: String = "sounddrop://oauth"//don't forget to change this in Info.plist as well"
+    let SC_TRACKS_API_URL: String = "https://api.soundcloud.com/tracks"
+    let API_URL: String = "http://appassets.spaceodigicom.com/Sounddrop/action"
+}

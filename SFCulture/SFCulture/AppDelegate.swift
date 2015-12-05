1b6271ccc7d82397bb19b9a2fa52e7b58c350892
JamStop <jimmyyue1@gmail.com>
2015-12-05 12:15:50 -0800

    probably don't need Facebook login

--- SFCulture/SFCulture/AppDelegate.swift
+++ SFCulture/SFCulture/AppDelegate.swift
@@ -21,15 +21,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
 
-        Parse.enableLocalDatastore()
-        Parse.setApplicationId("huCV5qt0PKqorf1aYHbv93jQo7nWZNdvZtP58t9w",
-            clientKey: "6NbhqNdcQiuQd7wGaXS3XFLnPYdGGTMc0CwQuVhz")
-        
-        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
-
-        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
-        
-        let user = PFUser.currentUser()
+//        Parse.enableLocalDatastore()
+//        Parse.setApplicationId("huCV5qt0PKqorf1aYHbv93jQo7nWZNdvZtP58t9w",
+//            clientKey: "6NbhqNdcQiuQd7wGaXS3XFLnPYdGGTMc0CwQuVhz")
+//        
+//        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+//
+//        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+//        
+//        let user = PFUser.currentUser()
         
         return true
     }

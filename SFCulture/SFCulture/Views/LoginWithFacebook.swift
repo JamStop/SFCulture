acec660a42d10be895c6cfa0e61b59bd06afe637
JamStop <jimmyyue1@gmail.com>
2015-12-07 12:41:14 -0800

    setup login

---
+++ SFCulture/SFCulture/Views/LoginWithFacebook.swift
@@ -0,0 +1,53 @@
+//
+//  LoginWithFacebook.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 12/6/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+@IBDesignable
+class LoginWithFacebook: UIView {
+    
+    var view: UIView!
+
+    @IBOutlet weak var cellButton: UIButton!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        xibSetup()
+    }
+    
+    
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        addSubview(view)
+        
+        cellButton.titleLabel?.numberOfLines = 1
+        cellButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        cellButton.titleLabel?.lineBreakMode = NSLineBreakMode.ByClipping
+        
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "LoginWithFacebook", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        return view
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        xibSetup()
+    }
+
+    
+}
+

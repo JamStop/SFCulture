0be7ae9fbcacc630d7d432af21f403706f5576a1
JamStop <jimmyyue1@gmail.com>
2015-12-16 13:16:05 -0800

    remove parse

---
+++ SFCulture/SFCulture/Helpers/Realm.swift
@@ -0,0 +1,10 @@
+//
+//  Realm.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 12/16/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import Foundation
+import RealmSwift

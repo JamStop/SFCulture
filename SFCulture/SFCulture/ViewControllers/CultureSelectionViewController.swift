b4df29747ad8b52b450d1d4c248fd617caf1b9ef
JamStop <jimmyyue1@gmail.com>
2015-12-17 14:36:39 -0800

    start culture selection

---
+++ SFCulture/SFCulture/ViewControllers/CultureSelectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CultureSelectionViewController.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 12/17/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import Foundation

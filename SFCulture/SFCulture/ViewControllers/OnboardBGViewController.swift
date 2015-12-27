f6a58755e514cfe065367fa353ab27825c9579c0
JamStop <jimmyyue1@gmail.com>
2015-12-26 21:00:14 -0800

    onboardbg

---
+++ SFCulture/SFCulture/ViewControllers/OnboardBGViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardBGViewController.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 12/22/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import UIKit
+
+class OnboardBGViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

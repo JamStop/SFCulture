6c33c3532b981cfd6d917cc63395cdd7f3c9a88c
JamStop <jimmyyue1@gmail.com>
2016-01-06 15:50:44 -0800

    tab bar set up

---
+++ SFCulture/SFCulture/Views/ProfileCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileCollectionViewCell.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 1/6/16.
+//  Copyright © 2016 Augmented Humanity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+#if !RX_NO_MODULE
+import RxSwift
+import RxCocoa
+#endif
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+}

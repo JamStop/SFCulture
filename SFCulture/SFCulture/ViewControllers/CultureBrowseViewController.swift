6f5483685c08d00a7c873bf89a2739291a982633
JamStop <jimmyyue1@gmail.com>
2016-01-11 20:07:33 -0800

    setup basic profile

---
+++ SFCulture/SFCulture/ViewControllers/CultureBrowseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CultureBrowseViewController.swift
+//  SFCulture
+//
+//  Created by Jimmy Yue on 1/11/16.
+//  Copyright © 2016 Augmented Humanity. All rights reserved.
+//
+
+import UIKit
+
+class CultureBrowseViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var profileCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
